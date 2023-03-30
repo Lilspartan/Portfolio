fd0e05ccbac595a5a4d5d6338c0e4cdd0b23d6bc
Gabe Krahulik <gabekrahulik@gmail.com>
2023-03-30 12:02:54 -0700

    respect prefers-reduced-motion

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,6 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet" />	
-          <link href="https://unpkg.com/movement.css/movement.css" rel="stylesheet" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         </Head>
         <body>
