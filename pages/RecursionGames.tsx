a6759cd847345804a7613975b485b269559558df
Gabe Krahulik <gabekrahulik@gmail.com>
2024-09-06 15:39:35 -0700

    recursion games easter egg

---
+++ pages/RecursionGames.tsx
@@ -0,0 +1,15 @@
+const RecursionGames = () => {
+	return (
+		<div className="
+			bg-background
+			h-screen
+            flex
+            flex-col
+            justify-center
+		">
+			<img src = "/qr.png" className = "rounded-lg mx-auto" />
+		</div>
+	)
+}
+
+export default RecursionGames;
